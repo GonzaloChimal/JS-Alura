e2cdab6e1f163e1b78d97ab319c55a3649e03799
Gonzalo Duran Chimal <duran.chimal.gonzalo.1992@gmail.com>
2022-07-11 12:41:16 -0500

    JS-Alura

--- service/client-service.js
+++ service/client-service.js
@@ -1,3 +1,34 @@
+//backticks
+const crearNuevaLinea = (nombre,email) => {
+    const linea = document.createElement("tr");
+    const contenido =`<tr>
+        <td class="td" data-td>${nombre}</td>
+        <td>{${email}}</td>
+        <td>
+            <ul class="table__button-control">
+                <li>
+                    <a
+                        href="../screens/editar_cliente.html"
+                        class="simple-button simple-button--edit"
+                    >Editar</a>
+                </li>
+                <li>
+                    <button
+                        class="simple-button simple-button--delete"
+                        type="button"
+                    >
+                        Eliminar
+                    </button>
+                </li>
+            </ul>
+        </td>
+    </tr>`;
+    linea.innerHTML = contenido;
+    return linea
+};
+
+const table = document.querySelector("[data-table]");
+
 const http = new XMLHttpRequest();
 
 //Abrir http (metodo, url);
@@ -10,5 +41,11 @@ const http = new XMLHttpRequest();
 
 
 http.open("GET","http://localhost:3000/perfil");
-
-console.log(http);
+http.send();
+http.onload = () => {
+    const data = JSON.parse(http.response);
+    data.forEach(perfil => {
+        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+        table.appendChild(nuevaLinea);      
+    });
+};
