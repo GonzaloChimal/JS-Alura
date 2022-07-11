4ccc2ea7ba7ba859a26220efefa47c656e020610
Gonzalo Duran Chimal <duran.chimal.gonzalo.1992@gmail.com>
2022-07-11 12:19:58 -0500

    JS-Alura

---
+++ service/client-service.js
@@ -0,0 +1,14 @@
+const http = new XMLHttpRequest();
+
+//Abrir http (metodo, url);
+
+//CRUD - Método HHTP
+//Create - POST
+//Read - GET 
+//Update - PUT/PATH 
+//Delete - DELETE
+
+
+http.open("GET","http://localhost:3000/perfil");
+
+console.log(http);
