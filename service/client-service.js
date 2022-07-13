3d981f34a9048dc2ab42cfe1c7486473eb04f2ab
Gonzalo Duran Chimal <duran.chimal.gonzalo.1992@gmail.com>
2022-07-13 14:51:00 -0500

    JS-Alura

--- service/client-service.js
+++ service/client-service.js
@@ -1,65 +1,58 @@
 //backticks
-const crearNuevaLinea = (nombre,email) => {
+const crearNuevaLinea = (nombre, email) => {
     const linea = document.createElement("tr");
-    const contenido =`<tr>
-        <td class="td" data-td>${nombre}</td>
-        <td>${email}</td>
-        <td>
-            <ul class="table__button-control">
-                <li>
-                    <a
-                        href="../screens/editar_cliente.html"
-                        class="simple-button simple-button--edit"
-                    >Editar</a>
-                </li>
-                <li>
-                    <button
-                        class="simple-button simple-button--delete"
-                        type="button"
-                    >
-                        Eliminar
-                    </button>
-                </li>
-            </ul>
-        </td>
-    </tr>`;
+    const contenido = `
+      <td class="td" data-td>
+        ${nombre}
+      </td>
+      <td>${email}</td>
+      <td>
+        <ul class="table__button-control">
+          <li>
+            <a
+              href="../screens/editar_cliente.html"
+              class="simple-button simple-button--edit"
+            >
+              Editar
+            </a>
+          </li>
+          <li>
+            <button class="simple-button simple-button--delete" type="button">
+              Eliminar
+            </button>
+          </li>
+        </ul>
+      </td>
+    `;
     linea.innerHTML = contenido;
-    return linea
-};
-
-const table = document.querySelector("[data-table]");
-
-//Abrir http (metodo, url);
-
-//CRUD - Método HHTP
-//Create - POST
-//Read - GET 
-//Update - PUT/PATH 
-//Delete - DELETE
-
-function listaCliente = () => {
-    const promise = new Promise((resolve,reject) = {
-        const http = new XMLHttpRequest();
-        http.open("GET","http://localhost:3000/perfil");
-        http.send();
-        http.onload = () => {
-            const data = JSON.parse(http.response);
-
-            if(http.status >= 400){
-                reject(response);
-            }
-            else{
-                resolve(response);
-            }
-        };
+    return linea;
+  };
+  
+  const table = document.querySelector("[data-table]");
+  
+  //Abrir http (método,url)
+  
+  // CRUD   - Métodos HTTP
+  // Create - POST
+  // Read   - GET
+  // Update - PUT/PATCH
+  // Delete - DELETE
+  
+  // Fetch API  
+  const listaClientes = () => {
+    return fetch("http//localhost:3000/perfil").then(respuesta => {
+        return respuesta.json();
     });
-
-    return promise;
-};
-
-
-
-data.forEach(perfil => {
-    const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-    table.appendChild(nuevaLinea);      
-});
+  };
+  
+  listaClientes()
+    .then((data) => {
+      data.forEach((perfil) => {
+        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+        table.appendChild(nuevaLinea);
+      });
+    })
+    .catch((error) => alert("Ocurrió un error"));
+  
+  // console.log(data);
+  //
